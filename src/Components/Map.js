3db64dfee465c5a6377d8810e7b3a67c2b3960a8
Talha2647 <33353192+Talha2647@users.noreply.github.com>
2022-10-15 16:43:56 +0200

    map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,5 +1,6 @@
 
 import { useState, useEffect,Suspense } from "react";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
 
 
@@ -11,7 +12,7 @@ export default function Map({country, ipAddressDict }) {
     useEffect(() => { 
         async function getData(){
         try {
-            const result = await fetch('https://restcountries.com/v3.1/all?access_token=')
+            const result = await fetch('https://restcountries.com/v3.1/all?access_token=at_uTanxU4QwfUvFHmUMgeDa5dtRVHsE')
             const data = await result.json()
             setData(data)
           } 
@@ -26,20 +27,25 @@ export default function Map({country, ipAddressDict }) {
     console.log (zoom)
     return (
 
-        country && ipAddressDict
-        ?<div className="bg-black flex flex-col" >
+      
           
          
-            <div className="h-100%">
+        
            
-              
-                        <Suspense fallback={null}>
-                            <Map x={30} y={30} />    
-                        </Suspense>
+        <MapContainer center={[48.1351, 11.5820]} zoom={3} scrollWheelZoom={false} style={{ width: "30%", height: "50px" }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      <Marker position={[48.1351, 11.5820]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker> 
+      </MapContainer>
+    
                 
-            </div>
-        </div>
-    :<p>loading...</p>
+ 
 
     )
             };
